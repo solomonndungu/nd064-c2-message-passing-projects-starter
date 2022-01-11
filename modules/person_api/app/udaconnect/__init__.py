cb03b8a71751d5110069c5770eb00fbfa2df1b3b
solomonndungu <ssouljabee@gmail.com>
2022-01-11 18:15:22 +0300

    person and connection yaml files

--- modules/person_api/app/udaconnect/__init__.py
+++ modules/person_api/app/udaconnect/__init__.py
@@ -1,4 +1,4 @@
 def register_routes(api, app, root="person_api"):
-    from app.udaconnect.controllers import api as udaconnect_person_api
+    from controllers import api as udaconnect_person_api
 
     api.add_namespace(udaconnect_person_api, path=f"/{root}")
