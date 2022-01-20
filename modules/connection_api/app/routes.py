38f0a29d65ac7786525c232483d7fecba1d4f4ce
solomonndungu <ssouljabee@gmail.com>
2022-01-20 11:36:33 +0300

    Dockerfile update and ports update

--- modules/connection_api/app/routes.py
+++ modules/connection_api/app/routes.py
@@ -1,4 +1,4 @@
-def register_routes(api, app, root="connection_api"):
+def register_routes(api, app, root="api"):
     from app.udaconnect import register_routes as attach_udaconnect
 
     # Add routes
--- modules/person_api/app/routes.py
+++ modules/person_api/app/routes.py
@@ -1,4 +1,4 @@
-def register_routes(api, app, root="person_api"):
+def register_routes(api, app, root="api"):
     from app.udaconnect import register_routes as attach_udaconnect
 
     # Add routes
